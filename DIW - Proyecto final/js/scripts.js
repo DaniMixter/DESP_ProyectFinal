08881ce98bcb0d297cc3cbd0cedd5f7911b91646
Daniel V. Ruiz Pérez <91469416+DaniMixter@users.noreply.github.com>
2022-02-08 18:34:23 +0100

    V.3.0
    Falta Jquery

--- DIW - Proyecto final/js/scripts.js
+++ DIW - Proyecto final/js/scripts.js
@@ -1,32 +1,40 @@
-$(document).ready(function() {
+$(document).ready(function () {
     carrito();
-    eliminarProducto(precioResta);
-    var precioResta = '0';
-
-    dinero(precioResta);
-
-});
-
-function carrito() {
-    var row_no = $('table tbody tr').length;
-    document.querySelector('#shopping-cart span').innerHTML = row_no;
-}
-
-function eliminarProducto(precioResta) {
-    $(document).on('click', '.eliminarFila', function() {
-        $(this).parent('td').parent('tr').remove();
-        var row_no = $('table tbody tr').length;
-        document.querySelector('#shopping-cart span').innerHTML = row_no;
-        precioResta = parseFloat($('.precios').text());
-
-    });
-}
-
-function dinero(precioResta) {
-
+    var precioResta = 0;
     var precio1 = parseFloat($('.precio').text());
     var precio2 = parseFloat($('.preci').text());
     var precio3 = parseFloat($('.PrecioEnvio').text());
     var precio = (precio1 + precio2 + precio3 - precioResta);
-    document.querySelector('table tfoot .PrecioF').innerHTML = precio;
+    dinero(precio);
+    eliminarProducto(precioResta, precio3);
+});
+function carrito() {
+    var row_no = $('table tbody tr').length;
+    document.querySelector('#shopping-cart span').innerHTML = row_no;
+    precioResta = parseFloat($('.precios').text());
+    precioResta = (precio3 + precioResta);
+    dinero(precioResta.toFixed(2));
+}
+/*function eliminarMoney(row_now){
+    if (row_now = 1) {
+        precioResta = parseFloat($('.precios').text());
+        precioResta = (precio3 + precioResta);
+        dinero(precioResta.toFixed(2));
+    }
+}*/
+function eliminarProducto(precioResta, precio3) {
+    $(document).on('click', '.eliminarFila', function () {
+        $(this).parent('td').parent('tr').remove();
+        carrito();
+    });
+}
+function cantidad(precio1, precio2) {
+    var cantidad1 = parseFloat($('.cantidad1').text());
+    var cantidad2 = parseFloat($('.cantidad2').text());
+    var c = precio1 * cantidad1;
+    var precioFinal2 = precio2 * cantidad2;
+
+}
+function dinero(precio) {
+    document.querySelector('table tfoot .PrecioF').innerHTML = precio + "&euro;";
 }
