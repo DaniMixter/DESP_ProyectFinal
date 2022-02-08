9eb07f5280a9bae6d5428cfa794a13298e170183
Daniel V. Ruiz Pérez <danirupee@gmail.com>
2022-02-08 13:26:52 +0100

    v2.2

--- DIW - Proyecto final/js/scripts.js
+++ DIW - Proyecto final/js/scripts.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
+    carrito();
+    eliminarProducto(precioResta);
+    var precioResta = '0';
+
+    dinero(precioResta);
+
+});
+
+function carrito() {
     var row_no = $('table tbody tr').length;
-    document.querySelector('#shopping-cart span').innerHTML = row_no - 1;
+    document.querySelector('#shopping-cart span').innerHTML = row_no;
+}
+
+function eliminarProducto(precioResta) {
     $(document).on('click', '.eliminarFila', function() {
         $(this).parent('td').parent('tr').remove();
         var row_no = $('table tbody tr').length;
-        document.querySelector('#shopping-cart span').innerHTML = row_no - 1;
-        var row_precio = $('table tbody tr .precio').length;
+        document.querySelector('#shopping-cart span').innerHTML = row_no;
+        precioResta = parseFloat($('.precios').text());
 
     });
+}
 
-    row_precio = document.querySelector('table tbody tr .precio').innerHTML;
-    row_precio2 = document.querySelector('table tbody tr .preci').innerHTML;
+function dinero(precioResta) {
 
-
-});
+    var precio1 = parseFloat($('.precio').text());
+    var precio2 = parseFloat($('.preci').text());
+    var precio3 = parseFloat($('.PrecioEnvio').text());
+    var precio = (precio1 + precio2 + precio3 - precioResta);
+    document.querySelector('table tfoot .PrecioF').innerHTML = precio;
+}
